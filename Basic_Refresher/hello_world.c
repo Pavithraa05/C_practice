3004e6a598dfab9a3adb4b81e48f84a8fd6853e2
Pavithra <paviithraa05@gmail.com>
2026-06-23 17:52:46 +0000

    Initial commit

---
+++ Basic_Refresher/hello_world.c
@@ -0,0 +1,8 @@
+/*C code*/  //header
+#include<stdio.h>  //preprocessor directive
+int main()         // start of the program
+{
+    /*To display Hello World*/   // comment
+    printf("Hello World!\n"); //statement
+    return 0;                 // program termination
+}
